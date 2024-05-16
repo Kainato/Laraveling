92858a3eabd056f54eb0bddefb5495532ae0c494
Caio Araújo <caiocaladaraujo@gmail.com>
2024-05-16 09:33:28 -0300

    10 | Ajuste de rotas na home page

--- resources/views/site/home.blade.php
+++ resources/views/site/home.blade.php
@@ -58,13 +58,13 @@
         <h2>Menu de aprendizado</h2>
     </div>
     <div class="rowButtonsContainer">
-        <a href="/" disabled selected>
+        <a href="/test/10/20" disabled selected>
             <button>Teste array associativo</button>
         </a>
-        <a href="/" disabled selected>
+        <a href="/test/500" disabled selected>
             <button>Teste compact</button>
         </a>
-        <a href="/" disabled selected>
+        <a href="/test/42/69/7" disabled selected>
             <button>Teste with</button>
         </a>
     </div>
@@ -76,13 +76,13 @@
     </div>
     <hr>
     <div class="rowButtonsContainer">
-        <a href="/test/10" disabled selected>
+        <a href="/parametros/1" disabled selected>
             <button>Teste parametros 1</button>
         </a>
-        <a href="/" disabled selected>
+        <a href="/parametros/1/Caio" disabled selected>
             <button>Teste parametros 2</button>
         </a>
-        <a href="/" disabled selected>
+        <a href="/parametros/1/Caio/Hello_World" disabled selected>
             <button>Teste parametros 3</button>
         </a>
     </div>
