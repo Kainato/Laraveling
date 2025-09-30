8ef3fba253ed87a672fbe60856665202daa63254
Kainato <caiocaladaraujo@gmail.com>
2025-09-30 11:22:47 -0300

    Remove URLs de exemplo das rotas no arquivo de rotas

--- routes/web.php
+++ routes/web.php
@@ -7,11 +7,9 @@ use Illuminate\Support\Facades\Route;
 // -----------------------------------------------------------------------------
 
 // # Página Inicial
-// URL: http://localhost:8000/
 Route::get('/', [App\Http\Controllers\HomeController::class, 'home'])->name('site.home');
 
 // Página de documentação do Laravel
-// URL: http://localhost:8000/laravel
 Route::get('/laravel', function () {
     return view('welcome');
 })->name('laravel');
@@ -37,7 +35,6 @@ Route::prefix('/auth')->group(function () {
 // Rotas da integração com o usuário do Banco MySQL
 Route::prefix('/user')->group(function () {
     // Listagem de usuários
-    // URL: http://localhost:8000/user/listagem
     Route::get('/list', [App\Http\Controllers\UserController::class, 'list'])->name('app.user.userlist');
 });
 
@@ -46,7 +43,6 @@ Route::prefix('/user')->group(function () {
 // -----------------------------------------------------------------------------
 
 // Página de Fallback (Página de erro)
-// URL: http://localhost:8000/{qualquer_rota_não_existente}
 Route::fallback(function () {
     echo 'A página acessada não existe. <a href="' . route('site.home') . '">Clique aqui</a> para voltar à página inicial.';
 });
