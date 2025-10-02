41d88a0f8b1ce3c156dfb90556332b8f839ebdd9
Kainato <caiocaladaraujo@gmail.com>
2025-10-02 16:14:22 -0300

    Instalando filament

---
+++ app/Filament/Resources/CharResource.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\CharResource\Pages;
+use App\Filament\Resources\CharResource\RelationManagers;
+use App\Models\Character;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class CharResource extends Resource
+{
+    protected static ?string $model = Character::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListChars::route('/'),
+            'create' => Pages\CreateChar::route('/create'),
+            'edit' => Pages\EditChar::route('/{record}/edit'),
+        ];
+    }
+}
